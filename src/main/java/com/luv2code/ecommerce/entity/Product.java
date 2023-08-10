0c4e378b53afc1730cc86f5ca167cee00700b39c
anvitha <anvithabandam@gmail.com>
2023-08-10 10:30:37 -0400

    Initial commit

---
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -0,0 +1,40 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unitPrice")
+    private BigDecimal unitPrice;
+    @Column(name = "imageUrl")
+    private String imageUrl;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "unitsInStock")
+    private int unitsInStock;
+    @Column(name = "dateCreated")
+    @CreationTimestamp
+    private int dateCreated;
+    @Column(name = "lastUpdated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
---
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,11 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_category")
+// @Data -- known bug
+public class ProductCategory {
+    
+}
